631ca0e0e178167b8994b2e7797d60caf1882186
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-24 12:33:25 -0700

    set up initial server routes

--- server.js
+++ server.js
@@ -15,6 +15,33 @@ app.listen(process.env.PORT || 5000, function (){
   console.log("listening on port 5000");
 });
 
+//root route
 app.get("/", function (req, res){
   res.send('We be jammin!')
 });
+
+//login check
+app.get('/users', function(req, res){
+  res.send('you just logged in');
+});
+
+//create a new user
+app.post('/users', function(req,res){
+  res.send('User Posted')
+});
+
+//view all to-dos
+app.get('/list', function(req,res){
+  res.send('this is where your posts will live')
+});
+
+//add a new todo
+app.post('/list', function(req,res){
+  res.send('you just created a new todo')
+})
+
+//remove a todo item from a user
+app.delete('/list/:id', function(req,res){
+  id = req.params.id;
+  res.send('you just deleted post: ' + id)
+})
