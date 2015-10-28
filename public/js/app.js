c8d91e4be0db6b38e52e5772c43ba1abefa5d1a8
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-28 15:25:53 -0700

    added client login/signup form  validation

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
 console.log('app.js loaded')
+
+
+$( document ).ready(function() {
+  //jquery validate sign in form validation
+  $(".form-signin").validate({
+    rules: {
+      password: {
+        required: true,
+        minlength: 8
+      },
+      email: {
+        required: true,
+      }
+    },
+    messages: {
+      email: {
+        required: "We need your email address to contact you, person!",
+      },
+      password: {
+        required: "You must enter a password",
+        minlength: jQuery.validator.format("Password must be at least {0} characters long!")
+      }
+    },
+    submitHandler: function(form) {
+      //submit if rules are met
+      form.submit();
+    }
+  });
+});
