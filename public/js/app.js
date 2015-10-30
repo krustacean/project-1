249a16d154710b1e8e58915b86490f8b25174226
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-30 08:53:50 -0700

    got client side script adding content as it reappears

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,10 @@ $( document ).ready(function() {
     })
     .done(function( msg ) {
      console.log(msg);
+     console.log(msg.todos[0].content);
+     var newPost = writePost(msg);
+     console.log(newPost)
+     $('.list-group').prepend(newPost);
    });
   });
 
@@ -73,4 +77,15 @@ function deletePost(context) {
       $(context).closest('li').remove();
     }
   });
+}
+
+function writePost(msg){
+  var userId = $('#user').data().id;
+  var postHtml = '<li><div class="panel panel-default"><div class="panel-body"><div class="button-group pull-right">'
+        + '<span class="badge">' + msg.todos[0].count + 'x</span>' + '<button type="button" name="repeat-button" id="repeat-button" class="btn btn-xs edit" data-id="' + userId + '">'
+        +  '<a href="/api/user/' + userId + '/todos/' + msg.todos[0]._id + '/repeat" method="put"><span class="glyphicon glyphicon-repeat"></span></a>'
+        + '</button> <button type="button" name="del-button" class="btn btn-xs delete" data-id="' + msg.todos[0]._id + '">'
+        +  '<span class="glyphicon glyphicon-remove"></span></button></div><p>' + msg.todos[0].content + '</p>'
+        + '<p><small><i>' + moment(msg.todos[0].timestamp).fromNow() + '</i></p></small></div></div></li>'
+  return postHtml;
 }
